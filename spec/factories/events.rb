051100b02288c27397481a8a6654171fad22fa78
Yaniv Savir <saviry@gmail.com>
2015-08-20 13:23:36 -0400

    event models have end dates

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :event do
     sequence(:name) { |n| "Event #{n}" }
     start_date '1/1/1620'
+    end_date '1/1/1620'
     summary 'This event\'s summary'
   end
 end
