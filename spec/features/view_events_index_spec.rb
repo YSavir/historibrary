7aca6f56274e5ebe2e043e81dcb3c7949b94aad2
Yaniv Savir <saviry@gmail.com>
2015-09-16 21:40:21 -0400

    adds end date to test to avoid failure

--- spec/features/view_events_index_spec.rb
+++ spec/features/view_events_index_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe 'When viewing the events index,', :js => true do
   context 'with a single event' do
     it 'should show a list of events with names and years' do
-      event = create :event, :name => 'Sample Event', :start_date => '2015'
+      event = create :event, :name => 'Sample Event',
+                             :start_date => '1/1/2015',
+                             :end_date => '1/1/2015'
        
       visit '/events'
 
       expect(page).to have_text 'Sample Event'
-      expect(page).to have_text '2015'
+      expect(page).to have_text '1/1/2015'
     end
   end
 end
