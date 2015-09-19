e4458969e1015fa84803159e052adccc40c9106a
Yaniv Savir <saviry@gmail.com>
2015-09-19 14:09:43 -0400

    add resource test without logged in user

--- spec/features/user_add_resource_to_event_spec.rb
+++ spec/features/user_add_resource_to_event_spec.rb
@@ -1,22 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe 'A logged-in user adding a resource to an event', :type => :feature, :js => true do
-  scenario 'should see that resource added to the event\'s resource list' do
-    user = create :user
-    resource_details = {name: "New Resource", summary: "new", source_url: 'url'}
-    create :event_with_resources
-    visit '/'
-    log_in_as user
+RSpec.describe 'As a user that is viewing an event\'s details', :type => :feature, :js => true do
+  context 'and that is logged in' do
+    context 'and I submit a resource' do
+      scenario 'I should see that resource added to the event\'s resource list' do
+      user = create :user
+      resource_details = {name: "New Resource", summary: "new", source_url: 'url'}
+      create :event_with_resources
+      visit '/'
+      log_in_as user
 
-    event_li = page.find('.content.events ul li')
-    event_li.click
-    event_li.click_button("Add a Resource")
-    form = page.find('.new-resource-form')
-    form.fill_in 'resource[name]', :with => resource_details[:name]
-    form.fill_in 'resource[summary]', :with => resource_details[:summary]
-    form.fill_in 'resource[source_url]', :with => resource_details[:source_url]
-    form.click_button 'Submit'
+      event_li = page.find('.content.events ul li')
+      event_li.click
+      event_li.click_button("Add a Resource")
+      form = page.find('.new-resource-form')
+      form.fill_in 'resource[name]', :with => resource_details[:name]
+      form.fill_in 'resource[summary]', :with => resource_details[:summary]
+      form.fill_in 'resource[source_url]', :with => resource_details[:source_url]
+      form.click_button 'Submit'
 
-    expect(event_li.find('li:last-of-type')).to have_text resource_details[:name]
+      expect(event_li.find('li:last-of-type')).to have_text resource_details[:name]
+      end
+    end
+
+  end
+  
+  context 'and that is not logged in' do
+    scenario 'I should not see a form to submit a new resource' do
+      user = create :user
+      create :event_with_resources
+      visit '/'
+
+      event_li = page.find('.content.events ul li')
+      event_li.click
+
+      expect(event_li).to have_no_content("Add a Resource")
+    end
   end
 end
