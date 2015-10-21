5a7239d73ae0b045c51ea73392387617d0feadc2
Yaniv Savir <saviry@gmail.com>
2015-10-21 13:16:14 -0400

    improve view looks and readability of events and resources

--- app/assets/javascripts/shared/backbone/models/resource-model.js
+++ app/assets/javascripts/shared/backbone/models/resource-model.js
@@ -1,1 +1,7 @@
-App.Models.Resource = Backbone.Model.extend({});
+App.Models.Resource = Backbone.Model.extend({
+
+  sourceName: function(){
+    return this.get('source_url').match(/(?:https?:\/\/)?(?:www\.)(.*)(\.com|\.net|\.org)/)[1];
+  }
+
+});
